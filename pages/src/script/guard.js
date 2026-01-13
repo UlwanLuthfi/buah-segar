e9dc19db5978272dbce0ca1eb36cfa13df514705
UlwanLuthfi <ulwanxluthfi@gmail.com>
2026-01-14 01:18:53 +0700

    fix: url bug

--- pages/src/script/guard.js
+++ pages/src/script/guard.js
@@ -2,13 +2,13 @@ const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
 if (!currentUser) {
   alert("Silakan login terlebih dahulu");
-  window.location.href = "/pages/login.html";
+  window.location.href = "login.html";
 }
 
 // Khusus halaman admin
 if (location.pathname.includes("/admin")) {
   if (currentUser.role !== "admin") {
     alert("Anda tidak memiliki akses admin");
-    window.location.href = "/pages/index.html";
+    window.location.href = "../index.html";
   }
 }
